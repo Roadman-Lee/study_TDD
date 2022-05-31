6a041726ae6a1ffb3be791054425c347ae66e129
Roadman <rdmshine@Roadmanui-iMac.local>
2022-05-31 23:03:31 +0900

    create lists app, refector config

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Study_TDD project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Study_TDD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+
+# class SmokeTest(TestCase):
+#
+#     def test_bad_maths(self):
+#         self.assertEqual(1+1, 3)
 
 
-class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    # 1. 특정 URL에 대한 HTTP Request를 받는다.
+    # 2. django는 특정 규칙을 이용해서 해당 요청에 어떤 뷰 함수를 실행할지 결정한다.(URL "해석"이라고 하는 처리)
+    # 3. 이 뷰 기능이 Request를 처리해서 HTTP Response로 반환한다.
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
